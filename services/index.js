fb1542bb296feb0142de00d6a41a5959d35ea4f8
kkeval <kevalpanchal58@gmail.com>
2021-12-23 13:03:05 +0530

    dasdfasf

--- services/index.js
+++ services/index.js
@@ -1,7 +1,7 @@
 import { request, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
-
+// sdfdsafasdfsadfsaf
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
