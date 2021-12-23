7036dee2cd98cde100831ae1cde3938709221daf
kkeval <kevalpanchal58@gmail.com>
2021-12-24 00:42:33 +0530

    changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-
-import Link from 'next/link';
-import { getCategories } from '../services';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { getCategories } from "../services";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
-
+  const [show, setshow] = useState(false);
   useEffect(() => {
     getCategories().then((newCategories) => {
       setCategories(newCategories);
@@ -13,20 +12,419 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
-          <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Curious Whale</span>
-          </Link>
-        </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
-          ))}
-        </div>
+    <>
+      <div className=" bg-white ">
+        <nav className="2xl:container 2xl:mx-auto sm:py-6 sm:px-7 py-5 px-4">
+          {/* For large and Medium-sized Screen */}
+          <div className="flex justify-between ">
+            <div className=" flex space-x-3 items-center">
+              <h1 className=" font-bold text-2xl leading-6 text-gray-800">
+                <Link href="/">Curious Whale</Link>
+              </h1>
+            </div>
+            <div className="hidden sm:flex flex-row space-x-4">
+              {/* //dropdown */}
+
+              <div className="dropdown relative ">
+                <button
+                  className="
+          dropdown-toggle
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg active:text-white
+          transition
+          duration-150
+          ease-in-out
+          flex
+          items-center
+          whitespace-nowrap
+        "
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Categories
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="caret-down"
+                    className="svg-inline--fa fa-caret-down w-2 ml-2"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                    ></path>
+                  </svg>
+                </button>
+                <ul
+                  className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none
+        "
+                >
+                  <li>
+                    <a
+                      className="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                      href="#"
+                    >
+                      Action
+                    </a>
+                  </li>
+                  {categories.map((category, index) => (
+                    <li key={index}>
+                      <a
+                        className="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                        href={`/category/${category.slug}`}
+                      >
+                        {category.name}
+                      </a>
+                    </li>
+                    // <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* Burger Icon */}
+            <div
+              id="bgIcon"
+              onClick={() => setshow(!show)}
+              className={`focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800  justify-center items-center sm:hidden cursor-pointer`}
+            >
+              <svg
+                className={`${show ? "hidden" : ""}`}
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className=" transform duration-150"
+                  d="M4 6H20"
+                  stroke="#1F2937"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M4 12H20"
+                  stroke="#1F2937"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  className=" transform duration-150"
+                  d="M4 18H20"
+                  stroke="#1F2937"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <svg
+                className={`${show ? "block" : "hidden"}`}
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M18 6L6 18"
+                  stroke="#1F2937"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M6 6L18 18"
+                  stroke="#1F2937"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+          {/* Mobile and small-screen devices (toggle Menu) */}
+          <div
+            id="MobileNavigation"
+            className={`${show ? "block" : "hidden"} sm:hidden mt-4 mx-auto`}
+          >
+            <div className="flex flex-col gap-4 mt-4 w-80 mx-auto ">
+              <div className="dropdown relative ">
+                <button
+                  className="
+          dropdown-toggle
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg active:text-white
+          transition
+          duration-150
+          ease-in-out
+          flex
+          items-center
+          whitespace-nowrap
+        "
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Categories
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="caret-down"
+                    className="svg-inline--fa fa-caret-down w-2 ml-2"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                    ></path>
+                  </svg>
+                </button>
+                <ul
+                  className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none
+        "
+                >
+                
+                  {categories.map((category, index) => (
+                    <li key={index}>
+                      <a
+                        className="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                        href={`/category/${category.slug}`}
+                      >
+                        {category.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
-    </div>
+      );
+      {/* <div className="container mx-auto px-10 mb-8">
+        <div className="border-b w-full inline-block border-blue-400 py-8">
+          <div className="md:float-left block">
+            <Link href="/">
+              <span className="cursor-pointer font-bold text-4xl text-white">
+                Curious Whale
+              </span>
+            </Link>
+          </div>
+
+          <div className="hidden md:float-left md:contents ">
+            <div className="flex justify-center md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <div className="">
+                <div className="dropdown relative ">
+                  <button
+                    className="
+          dropdown-toggle
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg active:text-white
+          transition
+          duration-150
+          ease-in-out
+          flex
+          items-center
+          whitespace-nowrap
+        "
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Categories
+                    <svg
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fas"
+                      data-icon="caret-down"
+                      className="svg-inline--fa fa-caret-down w-2 ml-2"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 320 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                      ></path>
+                    </svg>
+                  </button>
+                  <ul
+                    className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none
+        "
+                  >
+                    <li>
+                      <a
+                        className="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                        href="#"
+                      >
+                        Action
+                      </a>
+                    </li>
+                    {categories.map((category, index) => (
+                      <li key={index}>
+                        <a
+                          className="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                          href={`/category/${category.slug}`}
+                        >
+                          {category.name}
+                        </a>
+                      </li>
+                      // <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div> */}
+      {/* {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))} */}
+      {/* </div>
+        </div>
+      </div> */}
+    </>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import '../styles/globals.scss';
+import './header.css';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
   return (
+    
     <Layout>
       <Component {...pageProps} />
     </Layout>
